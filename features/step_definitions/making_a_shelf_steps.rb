200f84f3f8c5a3f1205f54495d4131d496a15b47
Matt Puchlerz <matt@puchlerz.com>
2010-04-28 09:50:09 -0500

    Simplified the steps to not use tables. Adjusted the step definitions accordingly.

--- features/step_definitions/making_a_shelf_steps.rb
+++ features/step_definitions/making_a_shelf_steps.rb
@@ -1,9 +1,9 @@
-Given /^a piece in the inventory that measures "([^\"]*)"$/ do |length|
-  pending # express the regexp above with the code you wish you had
+def get_pieces_from pieces_string
+  pieces_string.split(/\s*,\s*/).map { |piece| piece.to_i }
 end
 
-Given /^some pieces in the inventory that measure:$/ do |table|
-  # table is a Cucumber::Ast::Table
+Given /^(?:a piece|pieces) in the inventory that measures? "([^\"]*)"$/ do |pieces_string|
+  pieces = get_pieces_from pieces_string
   pending # express the regexp above with the code you wish you had
 end
 
@@ -11,24 +11,16 @@ When /^I make a shelf that measures "([^\"]*)"$/ do |length|
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^I should have a piece that measures "([^\"]*)"$/ do |count, length|
+Then /^I should have (?:a piece|pieces) that measures? "([^\"]*)"$/ do |pieces_string|
+  pieces = get_pieces_from pieces_string
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^I should have pieces that measure:$/ do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should have a piece left in the inventory that measures "([^\"]*)"$/ do |length|
+Then /^I should have (?:a piece|pieces) left in the inventory that measures? "([^\"]*)"$/ do |pieces_string|
+  pieces = get_pieces_from pieces_string
   pending # express the regexp above with the code you wish you had
 end
 
 Then /^I should have no pieces left in the inventory$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should have pieces left in the inventory that measure:$/ do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  Then 'I should have a piece left in the inventory that measures ""'
 end
